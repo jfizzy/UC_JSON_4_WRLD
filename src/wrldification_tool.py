cc8a7d0209b8dacb443f48cf61c585558487b6bb
James MacIsaac <james@jmproj.com>
2018-04-06 16:47:44 -0600

    Updated the script to edit some key features in the main.json fil

--- src/wrldification_tool.py
+++ src/wrldification_tool.py
@@ -4,6 +4,7 @@ import os
 from fnmatch import fnmatch
 import pathlib
 from collections import OrderedDict
+from enum import Enum
 
 
 print("Starting conversion...")
@@ -30,7 +31,8 @@ def wtype_switch(wtype):
         'door': 'door',
         'escalator': 'escalator',
         'bathroom': 'bathroom',
-        'wall': 'wall'
+        'wall': 'wall',
+        'pathway': 'pathway'
     }.get(wtype,'room')
 
 def removekey(d, key):
@@ -78,6 +80,8 @@ def alter_props(properties_dict):
                     name = 'Escalator'
                 elif wtype == 'stairs':
                     name = 'Stairs'
+                elif wtype == 'hallway':
+                    name = None
 
         except KeyError:
             pass
@@ -85,12 +89,54 @@ def alter_props(properties_dict):
     properties.update({'id': id, 'type': wtype, 'name': name}) # add the new ones
     return properties
 
+def order_levels(level_list):
+    sorted = []
+    for level in level_list:
+        if sorted:
+            this = LEVELS.get(level['id'].split('Level')[1])
+            for i in range(len(sorted)):
+                that = LEVELS.get(sorted[i]['id'].split('Level')[1])
+                if this < that:
+                    sorted.insert(i, level)
+                    break
+                elif this > that:
+                    if i == len(sorted)-1:
+                        sorted.append(level)
+                        break
+                    else:
+                        continue
+                else:
+                    pprint('found the same level twice!!!!')
+        else:
+            sorted = [level]
+
+    for i in range(len(sorted)):
+        sorted[i]['z_order'] = i
+
+    return sorted
+
+def fix_level_names(level_list):
+    for level in level_list:
+        if 'Level' in level['name']:
+            level['name'] = level['name'].split('Level')[1]
+        if not ' ' in level['readable_name']:
+            if 'Level' in level['readable_name']:
+                l = level['readable_name'].split('Level')[1]
+                level['readable_name'] = 'Level '+l
+    return level_list
+
 root = '.'
-pattern = "*.geojson"
+geo_pattern = "*.geojson"
+json_pattern = '*main.json'
+
+LEVELS = {'B3': 0, 'B2':1, 'B1':2, 'M1':3, 'G1':4, 'G1A':5, '01':6, '01A':7,
+          '02':8, '02A':9, '03':10, '03A':11, '04':12, '04A':13, '05':14,
+          '05A':15, '06':16, '06A':17, '07':18, '08':19, '09':20, '10':21,
+          '11':22, '12':23, 'P1':24, 'P2':25}
 
 for path, subdirs, files in os.walk(root):
     for name in files:
-        if fnmatch(name, pattern):
+        if fnmatch(name, geo_pattern) and not name.startswith('Path-'):
             with open(os.path.join(path, name), 'r') as f:
                 data = json.load(f)
                 feature_list = data['features']
@@ -106,6 +152,25 @@ for path, subdirs, files in os.walk(root):
             with open(os.path.join(path, name), 'w') as f:
                 json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '))
                 pprint('Wrote cleaned file: '+path+'/'+name)
+
+        elif fnmatch(name, json_pattern) and not name.startswith('Path-'):
+            with open(os.path.join(path, name), 'r') as f:
+                data = json.load(f)
+                level_list = data.get('levels')
+                level_list = order_levels(level_list) # orders the levels and applies the correct z order
+
+                #get the entrance level
+                entrance_index = 1
+                for level in level_list:
+                    if level['id'] == 'Level01':
+                        entrance_index = level['z_order']
+                data.update({'entrance_level': entrance_index})
+
+                level_list = fix_level_names(level_list)
+
+            with open(os.path.join(path, name), 'w') as f:
+                json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '))
+                pprint('Wrote cleaned main file: '+path+'/'+name)
 
 if __name__ == "__main__":
     pass
